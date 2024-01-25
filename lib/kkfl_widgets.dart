1eb1cb89f2b3ca20f993f52d0909a3e21d84bdf4
kittkat7 <rainbowtabby7@gmail.com>
2024-01-25 15:05:05 -0500

    Refactor

--- lib/kkfl_widgets.dart
+++ lib/kkfl_widgets.dart
@@ -12,4 +12,7 @@
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
 
-export './src/widgets/marked.dart';
+export './src/aspect.dart';
+export './src/marked.dart';
+export './src/popups.dart';
+export './src/simple_text.dart';
